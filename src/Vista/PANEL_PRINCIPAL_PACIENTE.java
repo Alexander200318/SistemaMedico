f5c1a46db72dc3e3e1e664a52df5f2a91a273cbb
MillyCalle <mauracalle19@gmail.com>
2024-07-23 17:58:04 -0500

    Paciente Interfaz

--- src/Vista/PANEL_PRINCIPAL_PACIENTE.java
+++ src/Vista/PANEL_PRINCIPAL_PACIENTE.java
@@ -74,111 +74,97 @@ public class PANEL_PRINCIPAL_PACIENTE extends javax.swing.JPanel {
         TxtIngresarNombreOCedula = new javax.swing.JTextField();
         jLabel1 = new javax.swing.JLabel();
         BtnBuscar = new javax.swing.JButton();
+        jSeparator2 = new javax.swing.JSeparator();
         panelRound5 = new Modelo.PanelRound();
         jScrollPane1 = new javax.swing.JScrollPane();
         TablaDatosPaciente = new javax.swing.JTable();
-        panelRound1 = new Modelo.PanelRound();
+        jSeparator1 = new javax.swing.JSeparator();
+        jPanel1 = new javax.swing.JPanel();
         Btn_NuevoPaciente = new javax.swing.JButton();
 
+        setBackground(new java.awt.Color(32, 65, 109));
+
+        Panel_camb_Registro.setBackground(new java.awt.Color(255, 255, 255));
+        Panel_camb_Registro.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
         panelRound4.setBackground(new java.awt.Color(255, 255, 255));
+        panelRound4.setBorder(javax.swing.BorderFactory.createTitledBorder(null, "", javax.swing.border.TitledBorder.DEFAULT_JUSTIFICATION, javax.swing.border.TitledBorder.DEFAULT_POSITION, new java.awt.Font("Maiandra GD", 1, 18))); // NOI18N
         panelRound4.setRoundBottomLeft(20);
         panelRound4.setRoundBottomRight(20);
         panelRound4.setRoundTopLeft(20);
         panelRound4.setRoundTopRight(20);
+        panelRound4.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
+        TxtIngresarNombreOCedula.setFont(new java.awt.Font("Maiandra GD", 0, 16)); // NOI18N
+        TxtIngresarNombreOCedula.setBorder(null);
+        TxtIngresarNombreOCedula.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                TxtIngresarNombreOCedulaActionPerformed(evt);
+            }
+        });
+        panelRound4.add(TxtIngresarNombreOCedula, new org.netbeans.lib.awtextra.AbsoluteConstraints(310, 30, 370, 30));
+
+        jLabel1.setFont(new java.awt.Font("Maiandra GD", 1, 18)); // NOI18N
         jLabel1.setText("Ingresa el nombre o la cedula:");
+        panelRound4.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(60, 30, -1, 30));
 
-        BtnBuscar.setText("Buscar");
+        BtnBuscar.setBackground(new java.awt.Color(64, 172, 159));
+        BtnBuscar.setFont(new java.awt.Font("Malgun Gothic Semilight", 1, 14)); // NOI18N
+        BtnBuscar.setForeground(new java.awt.Color(255, 255, 255));
+        BtnBuscar.setText("BUSCAR");
+        panelRound4.add(BtnBuscar, new org.netbeans.lib.awtextra.AbsoluteConstraints(700, 30, 140, 30));
+        panelRound4.add(jSeparator2, new org.netbeans.lib.awtextra.AbsoluteConstraints(310, 60, 370, 20));
 
-        javax.swing.GroupLayout panelRound4Layout = new javax.swing.GroupLayout(panelRound4);
-        panelRound4.setLayout(panelRound4Layout);
-        panelRound4Layout.setHorizontalGroup(
-            panelRound4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(panelRound4Layout.createSequentialGroup()
-                .addGap(23, 23, 23)
-                .addComponent(jLabel1)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(TxtIngresarNombreOCedula, javax.swing.GroupLayout.PREFERRED_SIZE, 328, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(29, 29, 29)
-                .addComponent(BtnBuscar)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-        );
-        panelRound4Layout.setVerticalGroup(
-            panelRound4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(panelRound4Layout.createSequentialGroup()
-                .addGap(22, 22, 22)
-                .addGroup(panelRound4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel1)
-                    .addComponent(TxtIngresarNombreOCedula, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(BtnBuscar))
-                .addContainerGap(186, Short.MAX_VALUE))
-        );
+        Panel_camb_Registro.add(panelRound4, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 30, 870, 90));
 
         panelRound5.setBackground(new java.awt.Color(255, 255, 255));
+        panelRound5.setBorder(javax.swing.BorderFactory.createTitledBorder(null, "REGISTRO DE PACIENTES EXISTENTES", javax.swing.border.TitledBorder.CENTER, javax.swing.border.TitledBorder.BELOW_TOP, new java.awt.Font("Maiandra GD", 1, 18), new java.awt.Color(32, 65, 109))); // NOI18N
         panelRound5.setRoundBottomLeft(20);
         panelRound5.setRoundBottomRight(20);
         panelRound5.setRoundTopLeft(20);
         panelRound5.setRoundTopRight(20);
+        panelRound5.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
+        TablaDatosPaciente.setFont(new java.awt.Font("Maiandra GD", 0, 14)); // NOI18N
         TablaDatosPaciente.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
+                {null, null, null, null, null},
+                {null, null, null, null, null},
+                {null, null, null, null, null},
+                {null, null, null, null, null},
+                {null, null, null, null, null},
+                {null, null, null, null, null},
+                {null, null, null, null, null},
+                {null, null, null, null, null},
+                {null, null, null, null, null},
+                {null, null, null, null, null},
+                {null, null, null, null, null},
+                {null, null, null, null, null},
+                {null, null, null, null, null},
+                {null, null, null, null, null},
+                {null, null, null, null, null},
+                {null, null, null, null, null},
                 {null, null, null, null, null},
                 {null, null, null, null, null},
                 {null, null, null, null, null},
                 {null, null, null, null, null}
             },
             new String [] {
-                "Nombre", "Apellido", "Sexo", "Celular", "Dirección"
+                "NOMBRE", "APELLIDO", "SEXO", "CELULAR", "DIRECCIÓN"
             }
         ));
         jScrollPane1.setViewportView(TablaDatosPaciente);
 
-        javax.swing.GroupLayout panelRound5Layout = new javax.swing.GroupLayout(panelRound5);
-        panelRound5.setLayout(panelRound5Layout);
-        panelRound5Layout.setHorizontalGroup(
-            panelRound5Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(panelRound5Layout.createSequentialGroup()
-                .addGap(19, 19, 19)
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 930, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(19, Short.MAX_VALUE))
-        );
-        panelRound5Layout.setVerticalGroup(
-            panelRound5Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(panelRound5Layout.createSequentialGroup()
-                .addGap(27, 27, 27)
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 335, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(111, Short.MAX_VALUE))
-        );
+        panelRound5.add(jScrollPane1, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 50, 1070, 360));
+        panelRound5.add(jSeparator1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 30, 1130, 10));
 
-        javax.swing.GroupLayout Panel_camb_RegistroLayout = new javax.swing.GroupLayout(Panel_camb_Registro);
-        Panel_camb_Registro.setLayout(Panel_camb_RegistroLayout);
-        Panel_camb_RegistroLayout.setHorizontalGroup(
-            Panel_camb_RegistroLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(Panel_camb_RegistroLayout.createSequentialGroup()
-                .addContainerGap()
-                .addGroup(Panel_camb_RegistroLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(panelRound4, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(panelRound5, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-                .addContainerGap())
-        );
-        Panel_camb_RegistroLayout.setVerticalGroup(
-            Panel_camb_RegistroLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(Panel_camb_RegistroLayout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(panelRound4, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(panelRound5, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(530, 530, 530))
-        );
+        Panel_camb_Registro.add(panelRound5, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 140, 1130, 430));
 
-        panelRound1.setBackground(new java.awt.Color(255, 255, 255));
-        panelRound1.setRoundBottomLeft(20);
-        panelRound1.setRoundBottomRight(20);
-        panelRound1.setRoundTopLeft(20);
-        panelRound1.setRoundTopRight(20);
+        jPanel1.setBackground(new java.awt.Color(255, 255, 255));
+        jPanel1.setBorder(javax.swing.BorderFactory.createTitledBorder(null, "", javax.swing.border.TitledBorder.DEFAULT_JUSTIFICATION, javax.swing.border.TitledBorder.DEFAULT_POSITION, new java.awt.Font("Maiandra GD", 0, 12))); // NOI18N
 
         Btn_NuevoPaciente.setBackground(new java.awt.Color(64, 172, 159));
-        Btn_NuevoPaciente.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        Btn_NuevoPaciente.setFont(new java.awt.Font("Malgun Gothic Semilight", 1, 14)); // NOI18N
         Btn_NuevoPaciente.setForeground(new java.awt.Color(255, 255, 255));
         Btn_NuevoPaciente.setText("Registra Paciente");
         Btn_NuevoPaciente.addActionListener(new java.awt.event.ActionListener() {
@@ -187,47 +173,50 @@ public class PANEL_PRINCIPAL_PACIENTE extends javax.swing.JPanel {
             }
         });
 
-        javax.swing.GroupLayout panelRound1Layout = new javax.swing.GroupLayout(panelRound1);
-        panelRound1.setLayout(panelRound1Layout);
-        panelRound1Layout.setHorizontalGroup(
-            panelRound1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(panelRound1Layout.createSequentialGroup()
-                .addGap(24, 24, 24)
-                .addComponent(Btn_NuevoPaciente, javax.swing.GroupLayout.PREFERRED_SIZE, 216, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(20, Short.MAX_VALUE))
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(16, 16, 16)
+                .addComponent(Btn_NuevoPaciente, javax.swing.GroupLayout.PREFERRED_SIZE, 200, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(22, Short.MAX_VALUE))
         );
-        panelRound1Layout.setVerticalGroup(
-            panelRound1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(panelRound1Layout.createSequentialGroup()
-                .addGap(20, 20, 20)
-                .addComponent(Btn_NuevoPaciente, javax.swing.GroupLayout.PREFERRED_SIZE, 41, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(589, Short.MAX_VALUE))
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(29, 29, 29)
+                .addComponent(Btn_NuevoPaciente, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(29, Short.MAX_VALUE))
         );
+
+        Panel_camb_Registro.add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(930, 32, 240, 90));
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(10, 10, 10)
-                .addComponent(Panel_camb_Registro, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(20, 20, 20)
-                .addComponent(panelRound1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(26, 26, 26)
+                .addComponent(Panel_camb_Registro, javax.swing.GroupLayout.PREFERRED_SIZE, 1222, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(24, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(10, 10, 10)
-                .addComponent(Panel_camb_Registro, javax.swing.GroupLayout.PREFERRED_SIZE, 660, javax.swing.GroupLayout.PREFERRED_SIZE))
-            .addGroup(layout.createSequentialGroup()
-                .addGap(20, 20, 20)
-                .addComponent(panelRound1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(29, 29, 29)
+                .addComponent(Panel_camb_Registro, javax.swing.GroupLayout.PREFERRED_SIZE, 615, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(26, Short.MAX_VALUE))
         );
     }// </editor-fold>//GEN-END:initComponents
 
     private void Btn_NuevoPacienteActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_Btn_NuevoPacienteActionPerformed
         // TODO add your handling code here:
     }//GEN-LAST:event_Btn_NuevoPacienteActionPerformed
+
+    private void TxtIngresarNombreOCedulaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_TxtIngresarNombreOCedulaActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_TxtIngresarNombreOCedulaActionPerformed
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
@@ -237,8 +226,10 @@ public class PANEL_PRINCIPAL_PACIENTE extends javax.swing.JPanel {
     private javax.swing.JTable TablaDatosPaciente;
     private javax.swing.JTextField TxtIngresarNombreOCedula;
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JPanel jPanel1;
     private javax.swing.JScrollPane jScrollPane1;
-    private Modelo.PanelRound panelRound1;
+    private javax.swing.JSeparator jSeparator1;
+    private javax.swing.JSeparator jSeparator2;
     private Modelo.PanelRound panelRound4;
     private Modelo.PanelRound panelRound5;
     // End of variables declaration//GEN-END:variables
