6d9c1c861b5901b17954d523136ced5b023ed082
Joel Pesantez <stalynpesantez@gmail.com>
2024-07-30 17:06:33 -0500

    asd
    ads

--- src/Modelo/Paciente.java
+++ src/Modelo/Paciente.java
@@ -19,8 +19,22 @@ public class Paciente extends Persona {
         this.pacEstActivo = pacEstActivo;
     }
 
-    
+    public Paciente(boolean pacEstActivo) {
+        this.pacEstActivo = pacEstActivo;
+    }
 
+    public Paciente(boolean pacEstActivo, int idPersona, String identificacion, String primNombre, String segNombre, String primApellido, String segApellido, String email, String direccion, String barrio, String canton, String provincia, String telefono, Date fechaNacimiento, String lugar, String pais, String genero, String estadoCivil, String sexo, byte[] foto, String etnia, Date fechaRegistro, String carnetConadis, boolean discapacidad, String tipoDiscapacidad, int porctDiscapacidad, String contactoEmergencia, boolean estadoActivo, String tipo_sangre) {
+        super(idPersona, identificacion, primNombre, segNombre, primApellido, segApellido, email, direccion, barrio, canton, provincia, telefono, fechaNacimiento, lugar, pais, genero, estadoCivil, sexo, foto, etnia, fechaRegistro, carnetConadis, discapacidad, tipoDiscapacidad, porctDiscapacidad, contactoEmergencia, estadoActivo, tipo_sangre);
+        this.pacEstActivo = pacEstActivo;
+    }
+
+    public Paciente(int idPaciente, int idPersona, String identificacion, String primNombre, String segNombre, String primApellido, String segApellido, String email, String direccion, String barrio, String canton, String provincia, String telefono, Date fechaNacimiento, String lugar, String pais, String genero, String estadoCivil, String sexo, byte[] foto, String etnia, Date fechaRegistro, String carnetConadis, boolean discapacidad, String tipoDiscapacidad, int porctDiscapacidad, String contactoEmergencia, boolean estadoActivo, String tipoSangre) {
+    super(idPersona, identificacion, primNombre, segNombre, primApellido, segApellido, email, direccion, barrio, canton, provincia, telefono, fechaNacimiento, lugar, pais, genero, estadoCivil, sexo, foto, etnia, fechaRegistro, carnetConadis, discapacidad, tipoDiscapacidad, porctDiscapacidad, contactoEmergencia, estadoActivo, tipoSangre);
+    this.idPaciente = idPaciente;
+    this.pacEstActivo = estadoActivo;
+}
+
+    
     // Constructor por defecto
     public Paciente() {
         super();
